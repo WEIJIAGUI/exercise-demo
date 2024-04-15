2bdfdf77c2a49181b7d104a26a63b2d45f693140
weijiagui <18322393187@163.com>
2024-04-15 22:08:24 +0800

    up

--- 数据结构/sort.js
+++ 数据结构/sort.js
@@ -20,5 +20,23 @@ function bubbleSort(arr) {
     }
   }
 }
-bubbleSort(arr);
-console.log(arr);
+// bubbleSort(arr);
+
+// 简单 快速排序
+function quickSort(arr) {
+  if (arr === null || arr.length === 0) return [];
+  let leader = arr[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (leader > arr[i]) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  left = quickSort(left);
+  right = quickSort(right);
+  return [...left, leader, ...right];
+}
+console.log(quickSort(arr));
